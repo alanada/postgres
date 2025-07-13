dfc567f77e481a886819b30e022bd347d5eba696
Alan Jeferson <alanada3400@gmail.com>
2025-07-13 09:31:52 -0400

    Add files via upload

--- init-data.sh
+++ init-data.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -e;
 
-# Cria a extensão pgvector no banco principal
+# Instalar dependências para compilar o pgvector
+apt-get update && apt-get install -y \
+  build-essential \
+  git \
+  postgresql-server-dev-14
+
+# Clonar e compilar o pgvector
+git clone --branch v0.7.0 https://github.com/pgvector/pgvector.git
+cd pgvector
+make && make install
+cd ..
+rm -rf pgvector
+
+# Criar a extensão no banco padrão
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS vector;
 EOSQL
+  
+# Aqui você pode adicionar outros comandos de inicialização se necessário
