4dd33e4b641fbb6b18af2649a49bd163c1da4da0
Alan Jeferson <alanada3400@gmail.com>
2025-07-13 09:54:45 -0400

    Update 03-create-extension.sh

--- 03-create-extension.sh
+++ 03-create-extension.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -e
 
+# Espera o Postgres iniciar
+until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
+  echo "Esperando o Postgres iniciar..."
+  sleep 2
+done
+
+# Cria extensão no banco chatwoot_nestor
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname chatwoot_nestor <<-EOSQL
   CREATE EXTENSION IF NOT EXISTS vector;
 EOSQL
+
+# Roda as migrations do Rails
+bundle exec rails db:migrate
+
+# Executa o comando padrão do container (ex: rails server)
+exec "$@"
